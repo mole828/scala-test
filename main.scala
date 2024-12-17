f60960af4fe1add90382f02c8bd818af967b58e0
mole828 <mrx800@163.com>
2024-12-17 18:16:14 +0800

    update

--- main.scala
+++ main.scala
@@ -1,14 +1,42 @@
+import scala.annotation.switch
+import scala.reflect.Selectable.reflectiveSelectable
+
 trait Barkable {
   def bark(): Unit
 }
 
 trait User extends Barkable
 
-object MainClass {
-  def main(args: Array[String]): Unit = {
-    val user = new User {
-      override def bark(): Unit = println("我可以叫")
-    }
-    user.bark()
-  }
+enum Dog extends Barkable {
+  case Husky extends Dog
+  case Poodle extends Dog
+
+  override def bark(): Unit = println("汪汪汪")
+}
+
+enum ProductType {
+    case Shipping
+    case Virtual
+}
+
+sealed trait Product {
+  val price: Double
+  val ptype: ProductType
+}
+
+class PhysicalProduct(override val price: Double) extends Product {
+    override val ptype: ProductType = ProductType.Shipping
+    val shippingCost: Double = 10.0
+}
+
+class VirtualProduct(override val price: Double ) extends Product {
+    override val ptype: ProductType = ProductType.Virtual
+}
+
+def vp(p: {val shippingCost: Double}): Double = p.shippingCost
+
+
+@main def main(): Unit = {
+    val product  = new PhysicalProduct(100.0)
+    println(vp(product))
 }
